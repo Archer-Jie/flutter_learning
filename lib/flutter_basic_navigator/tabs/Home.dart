cdaa7801f91703cbb27d1b9d2181ca39c28bb8c3
Archer <577159007@qq.com>
2020-02-13 23:59:19 +0800

    第九次更新

---
+++ lib/flutter_basic_navigator/tabs/Home.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import '../pages/Search.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        RaisedButton(
+          child: Text('跳转到搜索界面'),
+          color: Theme.of(context).accentColor,
+          textTheme: ButtonTextTheme.primary,
+          onPressed: (){
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context)=>SearchPage()
+              )
+            );
+          },
+        ),
+        SizedBox(height: 10,),
+
+      ],
+    );
+  }
+}
